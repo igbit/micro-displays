eaf1a130733f1059752818ee1e0921ba1740b0d2
igbitx <76784587+igbitx@users.noreply.github.com>
2020-12-30 16:40:49 +0000

    Update main240x240.py

--- main240x240.py
+++ main240x240.py
@@ -1,4 +1,4 @@
-#   Copyright 2020 ig https://github.com/igbit
+#   Copyright 2020 ig https://github.com/igbit igbitx@gmail.com
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
